c7d0eaa112b425e091f73ac3c7907b2ca4044093
Stafford Williams <stafford.williams@gmail.com>
2023-03-05 23:35:42 +1100

    add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.stafford.vite.capacitor',
+  appName: 'vite-and-capacitor',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
